978aaf9ac1b290e59fa6d307e6baaa5dcb72cf5c
Mrinal Seth <mseth01@dcs.bbk.ac.uk>
2013-02-17 19:22:04 +0000

    FutureMeeting Interface
    The interface for future meetings

---
+++ classes/FutureMeeting.java
@@ -0,0 +1,10 @@
+package classes;
+
+/**
+ * A meeting to be held in the future.
+ */
+
+public interface FutureMeeting {
+	// No methods here. This is just a naming interface
+	// (i.e. only necessary for type checking and/or downcasting)
+}
